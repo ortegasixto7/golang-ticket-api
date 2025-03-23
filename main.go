14aa0ab4690a3a8a1cd0f3a7db74c56698bbb615
Sixto Ortega <ortegasixto7@gmail.com>
2025-03-23 15:13:27 -0400

    feat: initial endpoints

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ortegasixto7/golang-ticket/src/controllers"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	var ticketCtrl controllers.TicketController
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/tickets", ticketCtrl.Health)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
