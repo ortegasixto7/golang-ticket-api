ca324e9789806feff2ff5072c8c96e016264a1ae
Sixto Ortega <ortegasixto7@gmail.com>
2025-04-19 17:04:17 -0400

    feat: add test

---
+++ src/integration/actions/signup/handler_test.go
@@ -0,0 +1,45 @@
+package signup
+
+import (
+	"testing"
+)
+
+func TestExecute_Validation(t *testing.T) {
+	tests := []struct {
+		name    string
+		request *Request
+		wantErr bool
+	}{
+		{
+			name: "Valid Request",
+			request: &Request{
+				Name:        "Test Name",
+				Description: "Test Description",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid Request - Missing Name",
+			request: &Request{
+				Description: "Test Description",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid Request - Missing Description",
+			request: &Request{
+				Name: "Test Name",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Execute(tt.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Execute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
